d71c21606a08e897b76465e96021e6a6180cd228
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-12-08 14:27:39 +0300

    Initial Commit

---
+++ SystemDataCourseWork/SystemDataCourseWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemDataCourseWorkApp.swift
+//  SystemDataCourseWork
+//
+//  Created by Александр Плешаков on 08.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SystemDataCourseWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
